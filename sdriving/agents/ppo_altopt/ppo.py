Numerical Stability Issues and Training fixes

sdriving/agents/ppo_altopt/ppo.py
@@ -113,7 +113,7 @@ class PPO_Alternating_Optimization_Centralized_Critic:
             self.actor = PPOWaypointCategoricalActor(**self.actor_params)
         elif isinstance(self.env.action_space[0], gym.spaces.Box):
             self.actor = PPOWaypointGaussianActor(**self.actor_params)
-        self.ac = PPOLidarActorCritic(**ac_kwargs)
+        self.ac = PPOLidarActorCritic(**self.ac_params)
 
         self.device = device
 
@@ -226,20 +226,20 @@ class PPO_Alternating_Optimization_Centralized_Critic:
         device = self.device
         clip_ratio = self.clip_ratio
 
-        obs, act, logp_old, ret, = [
-            data[k] for k in ["obs", "act", "logp", "ret"]
+        obs, act, logp_old, rew, = [
+            data[k] for k in ["obs", "act", "logp", "rew"]
         ]
 
         # Policy loss
         pi, _, logp = self.actor(obs, act)
         ratio = torch.exp(logp - logp_old)  # N x B
-        clip_adv = torch.clamp(ratio, 1 - clip_ratio, 1 + clip_ratio) * adv
-        loss_pi = -torch.min(ratio * adv, clip_adv).mean()
+        clip_rew = torch.clamp(ratio, 1 - clip_ratio, 1 + clip_ratio) * rew
+        loss_pi = -torch.min(ratio * rew, clip_rew).mean()
 
         # Entropy Loss
         ent = pi.entropy().mean()
 
-        loss = loss_pi - ent * self.entropy_coeff + value_loss
+        loss = loss_pi - ent * self.entropy_coeff
         self.entropy_coeff -= self.entropy_coeff_decay
 
         # Logging Utilities
@@ -455,6 +455,7 @@ class PPO_Alternating_Optimization_Centralized_Critic:
 
     def train(self):
         for epoch in range(self.epochs):
+            self.logger.store(Epoch=epoch)
             start_time = time.time()
             self.controller_episode_runner()
             self.logger.store(
@@ -483,9 +484,9 @@ class PPO_Alternating_Optimization_Centralized_Critic:
         o, ep_ret, ep_len = env.reset(), 0, 0
         o = env.step(0, self.actor.act(o.to(self.device), deterministic=True))
         prev_done = torch.zeros(env.nagents, 1, device=self.device).bool()
-        for _ in range(self.local_steps_per_epoch):
+        for t in range(self.local_steps_per_epoch):
             obs, lidar = o
-            actions, valf, log_probs = self.ac.step(
+            actions, val_f, log_probs = self.ac.step(
                 [obs.to(self.device), lidar.to(self.device)]
             )
             next_o, r, d, info = self.env.step(1, actions)
@@ -536,6 +537,8 @@ class PPO_Alternating_Optimization_Centralized_Critic:
                 )
 
     def spline_episode_runner(self):
+        env = self.env
+
         for _ in range(self.local_number_episodes):
             obs = env.reset()
             _, actions, log_probs = self.actor(obs)
@@ -549,12 +552,15 @@ class PPO_Alternating_Optimization_Centralized_Critic:
                 action = self.ac.act(
                     [t.to(self.device) for t in o], deterministic=True
                 )
-                o, r, d, _ = env.step(0, action)
+                o, r, d, _ = env.step(1, action)
                 done = d.all()
                 acc_reward += r
             ep_ret = acc_reward.mean()
 
             self.spline_buffer.store(
-                obs, actions, acc_reward.to(self.device), log_probs
+                obs.detach(),
+                actions.detach(),
+                acc_reward[:, 0].to(self.device).detach(),
+                log_probs.detach()
             )
             self.logger.store(EpisodeReturnSpline=ep_ret)

sdriving/environments/spline_dual_objective_env.py
@@ -49,6 +49,12 @@ class MultiAgentIntersectionSplineAccelerationDiscreteEnvironment(
             MultiAgentOneShotSplinePredictionEnvironment, self
         ).get_reward(*args, **kwargs)
 
+    def discrete_to_continuous_actions(self, action: torch.Tensor):
+        return self.action_list[action]
+    
+    def discrete_to_continuous_actions_v2(self, action: torch.Tensor):
+        return action
+
     @torch.no_grad()
     def step(
         self,
@@ -64,7 +70,7 @@ class MultiAgentIntersectionSplineAccelerationDiscreteEnvironment(
                 MultiAgentOneShotSplinePredictionEnvironment, self
             ).step(action, render, **render_kwargs)
 
-        action = self.discrete_to_continuous_actions(action)
+        action = self.discrete_to_continuous_actions_v2(action)
         action = action.to(self.world.device)
 
         vehicle = self.agents["agent"]

sdriving/logging.py
@@ -69,8 +69,8 @@ def colorize(string, color, bold=False, highlight=False):
     attr.append(str(num))
     if bold:
         attr.append("1")
-    sttr = ";".join(attr)
-    return f"\x1b[{attr}m{string}\x1b[0"
+    attr = ";".join(attr)
+    return f"\x1b[{attr}m{string}\x1b[0m"
 
 
 class Logger:
@@ -172,7 +172,7 @@ class Logger:
             keystr = "%" + "%d" % max_key_len
             fmt = "| " + keystr + "s | %15s |"
             n_slashes = 22 + max_key_len
-            print("-" * n_slashes)
+            print("-" * n_slashes, flush=True)
             for key in self.log_headers:
                 val = self.log_current_row.get(key, "")
                 valstr = "%8.3g" % val if hasattr(val, "__float__") else val
@@ -259,7 +259,7 @@ class EpochLogger(Logger):
                 if isinstance(v[0], np.ndarray) and len(v[0].shape) > 0
                 else v
             )
-            vals = torch.as_tensor(vals)
+            vals = torch.as_tensor(vals).float()
             if with_min_and_max:
                 stats = hvd_scalar_statistics_with_min_max(vals)
             else:

sdriving/tsim/dynamics.py
@@ -205,7 +205,7 @@ class _SplineModel(nn.Module):
         diff = self.motion.diff
         ratio = diff[:, :, 1] / (diff[:, :, 0] + EPS)
         self.arc_lengths = self.motion.arc_lengths
-        self.curve_lengths = self.motion.curve_length.unsqueeze(1)
+        self.curve_lengths = self.motion.curve_length.unsqueeze(1) - 1e-3
         self.theta = angle_normalize(
             torch.where(
                 diff[:, :, 0] > 0,
@@ -232,22 +232,26 @@ class _SplineModel(nn.Module):
             self.distances + v * dt
         ) % self.curve_lengths  # N x 1
 
-        c1 = self.arc_lengths  # N x P
-        c2 = torch.cat(
-            [self.arc_lengths[:, 1:], self.arc_lengths[:, 0:1]], dim=-1
-        )  # N x P
+        c1 = self.arc_lengths[:, :-1]  # N x (P - 1)
+        c2 = self.arc_lengths[:, 1:]  # N x (P - 1)
         sgs = torch.where(
-            (c1 <= self.distances) * (self.distances < c2)
+            (c1 <= self.distances) * (self.distances <= c2)
         )  # (N x 1, N x 1)
+        
+        try:
+            ts = self.motion(self.distances, sgs)  # N x 1
+            pts = self.motion.sample_points(ts).reshape(-1, 2)  # N x 2
 
-        ts = self.motion(self.distances, sgs)  # N x 1
-        pts = self.motion.sample_points(ts).reshape(-1, 2)  # N x 2
+            theta = self.theta[sgs[0], sgs[1]].reshape(pts.size(0), 1)
+            v = torch.min(
+                torch.max(v + acceleration * dt, self.v_lim_neg), self.v_lim
+            )
+            return torch.cat([pts, v, theta], dim=1)
+        except:
+            print(self.distances)
+            print(self.arc_lengths)
 
-        theta = self.theta[sgs[0], sgs[1]].reshape(pts.size(0), 1)
-        v = torch.min(
-            torch.max(v + acceleration * dt, self.v_lim_neg), self.v_lim
-        )
-        return torch.cat([pts, v, theta], dim=1)
+        return
 
 
 def SplineModel(*args, **kwargs):